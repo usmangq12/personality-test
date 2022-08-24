53af5fbc6624265ddc275b94831b47f124957982
usmangq12 <usmangq12@gmail.com>
2022-08-24 17:48:20 +0500

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import FormComponent from "./Components/FormComponent";
+import FormComponent from "./Components/QuestionsContainer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./Components/Home";
 import { Result } from "./Components/Result";
